e6c5031c3ed000a595bba451d471461c9b1c2026
FlackCode <fl4ckc0de@gmail.com>
2024-12-26 16:09:46 +0200

    Made a ghost appear

--- game.js
+++ game.js
@@ -16,11 +16,19 @@ let wallOffset = (blockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
 let foodColor = "#FEB897";
 let score = 0;
+let ghosts = [];
 
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_DOWN = 1;
+
+let ghostLocations = [
+    {x: 0, y: 0},
+    {x: 176, y: 0},
+    {x: 0, y: 121},
+    {x: 176, y: 121},
+]
 
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -74,12 +82,19 @@ let drawScore = () => {
     canvasContext.fillText("Score: " + score, 0, blockSize * (map.length + 1));
 }
 
+let drawGhosts = () => {
+    for (let i = 0; i < ghosts.length; i++) {
+        ghosts[i].draw();
+    }
+}
+
 let draw = () => {
     createRect(0, 0, canvas.width, canvas.height, "#000000");
     drawWalls();
     drawFoods();
     pacman.draw();
     drawScore();
+    drawGhosts();
 }
 
 let gameInterval = setInterval(gameLoop, 1000/fps);
@@ -110,7 +125,27 @@ let createNewPacman = () => {
     pacman = new PacMan(blockSize, blockSize, blockSize, blockSize, blockSize / 5);
 }
 
+let createGhosts = () => {
+    ghosts = [];
+    for (let i = 0; i < 1; i++) {
+        let newGhost = new Ghost(
+            9 * blockSize + (i%2 == 0 ? 0 : 1) * blockSize,
+            10 * blockSize + (i%2 == 0 ? 0 : 1) * blockSize,
+            blockSize,
+            blockSize,
+            pacman.speed / 2,
+            ghostLocations[i % 4].x,
+            ghostLocations[i % 4].y,
+            124, 
+            116, 
+            6 + i,
+        );
+        ghosts.push(newGhost);
+    }
+}
+
 createNewPacman();
+createGhosts();
 gameLoop();
 
 window.addEventListener("keydown", (event) => {
---
+++ ghost.js
@@ -0,0 +1,119 @@
+class Ghost {
+    constructor(x, y, width, height, speed, imageX, imageY, imageWidth, imageHeight, range) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = DIRECTION_RIGHT;
+        this.imageX = imageX;
+        this.imageY = imageY;
+        this.imageHeight = imageHeight;
+        this.imageWidth = imageWidth;
+        this.range = range;
+
+    }
+
+    moveProcess() {
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if (this.checkCollision()) {
+            this.moveBackwards();
+        }
+    }
+
+    eat() {
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[0].length; j++) {
+                if(map[i][j] == 2 && this.getMapX() == j && this.getMapY() == i) {
+                    map[i][j] = 3;
+                    score++;
+                }
+            }
+        }
+    }
+
+    moveBackwards() {
+        switch(this.direction) { 
+            case DIRECTION_RIGHT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y += this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_DOWN:
+                this.y -= this.speed;
+                break;
+        }
+    }
+    moveForwards() {
+        switch(this.direction) { 
+            // the direction where the pacman will move, since the x plane goes -1 to 1 from left to right and y plane goes from -1 to 1 from top to bottom
+            case DIRECTION_RIGHT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y -= this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_DOWN:
+                this.y += this.speed;
+                break;
+        }
+    }
+    checkCollision() {
+        if(
+            map[this.getMapY()][this.getMapX()] == 1 ||
+            map[this.getMapYRightSide()][this.getMapX()] == 1 ||
+            map[this.getMapY()][this.getMapXRightSide()] == 1 ||
+            map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+    checkGhostCollision() {
+
+    }
+    changeDirectionIfPossible() {
+        if (this.direction == this.nextDirection) return;
+
+        let tempDirection = this.direction;
+        this.direction = this.nextDirection;
+        this.moveForwards();
+        if(this.checkCollision()) {
+            this.moveBackwards();
+            this.direction = tempDirection;
+        } else {
+            this.moveBackwards();
+        }
+    }
+    changeAnimation() {
+        this.currentFrame = 
+            this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
+    }
+    draw() {
+        canvasContext.save();
+        
+        canvasContext.drawImage(ghostFrames, this.imageX, this.imageY, this.imageWidth, this.imageHeight, this.x, this.y, this.width, this.height)
+        canvasContext.restore();
+    }
+    getMapX() {
+        return parseInt(this.x / blockSize);
+    }
+    getMapY() {
+        return parseInt(this.y / blockSize);
+    }
+    getMapXRightSide() {
+        return parseInt((this.x + 0.999 * blockSize) / blockSize);
+    }
+    getMapYRightSide() {
+        return parseInt((this.y + 0.999 * blockSize) / blockSize);
+    }
+}
